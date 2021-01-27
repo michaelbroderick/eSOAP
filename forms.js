4076de5655e5b42bde811ee66e178cd561236f7b
Michael Broderick <brodermi@tcd.ie>
2021-01-27 15:51:26 +0000

    RIF module + first bit of JS

---
+++ forms.js
@@ -0,0 +1,9 @@
+const otherComplaintBtn = document.querySelector('#presentingCheckR4_9');
+
+const otherComplaintBox = document.querySelector('#other_complaint_area');
+
+const toggleOther = () => {
+    otherComplaintBox.classList.toggle('input_visibility');
+}
+
+otherComplaintBtn.addEventListener('click', toggleOther);
